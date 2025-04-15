b6fa82bade9549e57d1ee7930c510277adb2e665
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-15 03:06:04 -0500

    Agregar componente From y actualizar estructura de App para incluir encabezado y sección

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import From from "./components/From"
+
 function App() {
 
   return (
     <>
-      <h1>Contador de calorías</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h2 className="text-center text-lg font-bold text-white uppercase">
+            Contador de calorías
+          </h2>
+          <button>
+            Reinicar
+          </button>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <From
+
+          />
+        </div>
+      </section >
     </>
   )
 }
---
+++ src/components/From.tsx
@@ -0,0 +1,5 @@
+export default function From() {
+    return (
+        <div>From</div>
+    )
+}

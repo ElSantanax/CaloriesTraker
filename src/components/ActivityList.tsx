41f0f407a770c32ceaf55ff8af21e339dc69f63d
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-19 19:55:25 -0500

    Refactor ActivityList para usar useMemo en el manejo de categorías y eliminar console.log

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,6 @@
 import { Activity } from "../types"
+import { categories } from "../data/db";
+import { useMemo } from "react";
 
 type ActivityListProps = {
     activities: Activity[]
@@ -6,26 +8,27 @@ type ActivityListProps = {
 
 export default function ActivityList({ activities }: ActivityListProps) {
 
-    console.log(activities);
+    const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
+        , [categories])
 
     return (
-
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
 
             {activities.map(activity => (
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className=" space-y-2 relative">
-                        <p>{activity.category}</p>
+                        <p>
+                            {categoryName(+activity.category)}
+                        </p>
                         <p className=" text-2xl font-bold pt-5">{activity.name}</p>
                         <p className=" font-black text-4xl text-lime-500">{activity.calories} {''} <span>Calorias</span></p>
                     </div>
                     <div>
 
-                    </div>
+                    </div>  
                 </div>
             ))}
-
         </>
     )
 }

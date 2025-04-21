3fcf358a8f8876c66372fc6cfc323e2dbe2be553
ElSantanax <josesantanaserrano130598@gmail.com>
2025-04-21 15:35:17 -0500

    Agregar componente CalorieTraker y su integración en App para mostrar actividades

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer, useEffect, useMemo } from "react"
 import From from "./components/From"
 import { activityReducer, initialState } from "./reducers/activity-reducers"
 import ActivityList from "./components/ActivityList"
+import CalorieTraker from "./components/CalorieTraker"
 
 function App() {
 
@@ -38,6 +39,14 @@ function App() {
           />
         </div>
       </section >
+
+      <section className="bg-gray-800 py-10">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTraker
+            activities={state.activities}
+          />
+        </div>
+      </section>
 
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList
---
+++ src/components/CalorieTraker.tsx
@@ -0,0 +1,11 @@
+import { Activity } from "../types"
+
+type CalorieTrakerProps = {
+    activities: Activity[]
+}
+
+export default function CalorieTraker({ activities }: CalorieTrakerProps) {
+    return (
+        <div>CalorieTraker</div>
+    )
+}
